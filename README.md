cd23aa8420cf7f528faf05d147b2c17f7732f472
Nuno Valadão <111917125+nunovaladao@users.noreply.github.com>
2023-07-15 17:19:33 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Simple_API_Python
-Project to learn about API's
+# Book Management API 🛠️📚
+
+## Introduction
+- The Book Management API is a Flask-based web application that provides endpoints for managing a collection of books. It allows users to perform operations such as retrieving all books, retrieving a specific book by its ID, creating a new book, updating an existing book, and deleting a book. 
+- I made this project just for fun to learn about API's.
+
+## Installation
+
+1. Make sure you have Python 3 installed on your system.
+2. Clone this repository to your local machine:
+``` bash
+git clone https://github.com/nunovaladao/Simple_API_Python.git
+```
+3. Navigate to the project directory:
+``` bash
+cd Simple_API_Python
+```
+4. Install the required dependencies using pip - pip3 if you're in a mac:
+``` bash
+pip3 install flask
+pip3 install requests
+``` 
+
+## Usage
+
+1. Run the Flask application:
+``` bash
+python app.py
+``` 
+2. Interact with the API using a tool like **Postman** or a **web browser**. Below are the available endpoints:
+- `GET /books: Retrieve all books`
+- `GET /books/<id>: Retrieve a book by its ID.`
+- `PUT /books/<id>: Update a book by its ID.`
+- `POST /books: Create a new book`
+- `DELETE /books/<id>: Delete a book by its ID.`
+**Note:** Replace <id> with the actual ID of the book.
+
+##
+Nuno Valadão | nsoares-@student.42porto.com
---
+++ app.py
@@ -0,0 +1,103 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+books = [
+    {
+        'id': 1,
+        'título': 'O Senhor dos Anéis - A Sociedade do Anel',
+        'Autor': 'J.R.R Tolkien'
+    },
+    {
+        'id': 2,
+        'título': 'Harry Potter e a Pedra Filosofal',
+        'Autor': 'J.K Howling'
+    },
+    {
+        'id': 3,
+        'título': 'James Clear',
+        'Autor': 'Hábitos Atômicos'
+    },
+    {
+        'id': 4,
+        'título': 'Rich Dad, Poor Dad',
+        'Autor': 'Robert Kiyosaki'
+    },
+    {
+        'id': 5,
+        'título': 'Icebreaker',
+        'Autor': 'Hannah Grace'
+    },
+]
+
+@app.route('/books', methods=['GET'])
+def consult_books():
+    """
+    Consults all the books.
+    
+    Returns:
+        JSON: A JSON object containing all the books.
+    """
+    return jsonify(books)
+
+@app.route('/books/<int:id>', methods=['GET'])
+def get_book(id):
+    """
+    Retrieves a book by its ID.
+    
+    Args:
+        id (int): The ID of the book to retrieve.
+    
+    Returns:
+        JSON: A JSON object containing the book information.
+    """
+    for book in books:
+        if book.get('id') == id:
+            return jsonify(book)
+
+@app.route('/books/<int:id>', methods=['PUT'])
+def edit_book(id):
+    """
+    Edits a book by its ID.
+    
+    Args:
+        id (int): The ID of the book to edit.
+    
+    Returns:
+        JSON: A JSON object containing the updated book information.
+    """
+    altered_book = request.get_json()
+    for index, book in enumerate(books):
+        if book.get('id') == id:
+            books[index].update(altered_book)
+            return jsonify(books[index])
+
+@app.route('/books', methods=['POST'])
+def new_book():
+    """
+    Creates a new book.
+    
+    Returns:
+        JSON: A JSON object containing the updated list of books.
+    """
+    new_book = request.get_json()
+    books.append(new_book)
+    return jsonify(books)
+
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    """
+    Deletes a book by its ID.
+    
+    Args:
+        id (int): The ID of the book to delete.
+    
+    Returns:
+        JSON: A JSON object containing the updated list of books.
+    """
+    for index, book in enumerate(books):
+        if book.get('id') == id:
+            del books[index]
+    return jsonify(books)
+
+app.run(port=5001, host='localhost', debug=True)
